cf8b208f25e04d235bb6edbf17378dad073ff72f
Djimi02 <58917799+Djimi02@users.noreply.github.com>
2022-01-25 18:05:32 +0100

    Add files via upload

--- backend/src/models/accident.py
+++ backend/src/models/accident.py
@@ -1,30 +1,44 @@
-class Accident:
+from peewee import *
 
-    def __init__(self, le_osgr, ln_osgr, long, lat, sev, vehicles, casualties, date, day_of_week, time, authority_district, authority_highway, first_road_class, first_road_number, road_type, speed_limit, junction_detail, junction_control, second_road_class, second_road_number, light, weather, road_surface, urban_or_rural, casualty_sev, casualty_type, junction_location):
-        self.le_osgr = le_osgr
-        self.ln_osgr =ln_osgr
-        self.long=long
-        self.lat=lat
-        self.sev=sev
-        self.vehicles=vehicles
-        self.casualties=casualties
-        self.date=date
-        self.day_of_week=day_of_week
-        self.time=time
-        self.authority_district=authority_district
-        self.authority_highway=authority_highway
-        self.first_road_class=first_road_class
-        self.first_road_number=first_road_number
-        self.road_type=road_type
-        self.speed_limit=speed_limit
-        self.junction_detail=junction_detail
-        self.junction_control=junction_control
-        self.second_road_class=second_road_class
-        self.second_road_number=second_road_number
-        self.light=light
-        self.weather=weather
-        self.road_surface=road_surface
-        self.urban_or_rural=urban_or_rural
-        self.casualty_sev=casualty_sev
-        self.casualty_type=casualty_type
-        self.junction_location=junction_location
+database = PostgresqlDatabase('RoadAccidentsDB', **{'user': 'postgres', 'password': 'mitkopostgres'})
+
+class UnknownField(object):
+    def __init__(self, *_, **__): pass
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+class Roadaccidentstable(BaseModel):
+    _1st_road_class = IntegerField(column_name='1st_Road_Class', null=True)
+    _1st_road_number = IntegerField(column_name='1st_Road_Number', null=True)
+    _2nd_road_class = IntegerField(column_name='2nd_Road_Class', null=True)
+    _2nd_road_number = IntegerField(column_name='2nd_Road_Number', null=True)
+    local_authority___district_ = IntegerField(column_name='Local_Authority_(District)', null=True)
+    local_authority___highway_ = CharField(column_name='Local_Authority_(Highway)', null=True)
+    accident_severity = IntegerField(null=True)
+    casualty_severity = IntegerField(null=True)
+    casualty_type = IntegerField(null=True)
+    date = DateField(null=True)
+    day_of_week = IntegerField(null=True)
+    junction_control = IntegerField(null=True)
+    junction_detail = IntegerField(null=True)
+    junction_location = IntegerField(null=True)
+    latitude = DoubleField(null=True)
+    light_conditions = IntegerField(null=True)
+    location_easting_osgr = IntegerField(null=True)
+    location_northing_osgr = IntegerField(null=True)
+    longitude = DoubleField(null=True)
+    number_of_casualties = IntegerField(null=True)
+    number_of_vehicles = IntegerField(null=True)
+    road_surface_conditions = IntegerField(null=True)
+    road_type = IntegerField(null=True)
+    speed_limit = IntegerField(null=True)
+    time = TimeField(null=True)
+    urban_or_rural_area = IntegerField(null=True)
+    weather_conditions = IntegerField(null=True)
+
+    class Meta:
+        table_name = 'roadaccidentstable'
+        primary_key = False
+
